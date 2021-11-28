1d21f892c2fedb2a0c3ef8d96344fe4d53eee4ce
cacexp <cacexp@gmail.com>
2021-11-28 11:29:07 +0100

    Create django project for testing

---
+++ test_resources/test_server/django/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from users import views
+
+urlpatterns = [
+    path('users/<int:id>/', views.UserView.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ test_resources/test_server/django/users/views.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+class UserView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    content_type="application/json"
+
+    @csrf_exempt
+    def get(self, request, id, format=None):
+        data = {
+            "name": "John",
+            "surname": "Smith"
+        }
+        response = JsonResponse(data, status=200)
+        response.set_cookie("was_here", "Yesyou", expires="Wed, 15-Nov-23 09:13:29 GMT")
+        return response
