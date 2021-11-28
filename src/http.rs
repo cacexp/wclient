60bccbca5de85e6020a2f12ecf6c3e6e0a1dee13
cacexp <cacexp@gmail.com>
2021-11-28 11:28:29 +0100

    Fix hostname

--- src/http.rs
+++ src/http.rs
@@ -186,7 +186,7 @@ impl ClientConnection  {
         if endpoint.default {
             write!(writer, "Host: {}\r\n", &endpoint.host)?;
         } else {
-            write!(writer, "Host: {:?}:{:?}\r\n", &endpoint.host, endpoint.port)?;
+            write!(writer, "Host: {}:{}\r\n", &endpoint.host, endpoint.port)?;
         }
     
         if request.cookies.len() > 0 {
